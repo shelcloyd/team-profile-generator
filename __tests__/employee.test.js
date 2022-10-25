a7cc9c714baac6b7e27225c9918f40ac7c7b10b9
Shel Cloyd <shel.cloyd@gmail.com>
2022-10-24 21:38:40 -0700

    rewrote employee test - it's passing now

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,24 +1,33 @@
 const Employee = require("../lib/employee");
 const shel = new Employee("Shel", "123456789", "none@none.com");
 
-describe("Employee", () => {
-    describe("data", () => {
+describe("Employee class", () => {
+    describe("initialization", () => {
         it("should return the data passed into the constructor", () => {
-            expect(employee.name).toBe("Shel");
-            expect(employee.id).toBe("123456789");
-            expect(employee.email).toBe("none@none.com");
+            expect(shel.name).toBe("Shel");
+            expect(shel.id).toBe("123456789");
+            expect(shel.email).toBe("none@none.com");
         });
-        it("should return the employee name from the getName() method", () => {
-            expect(employee.getName()).toBe("Shel");
+    });
+    describe("getName()", () => {
+        it("should return the employee name", () => {
+            expect(shel.getName()).toBe("Shel");
         });
-        it("should return the employee id from the getId() method", () => {
-            expect(employee.getId()).toBe("123456789");
+    });
+    describe("getId()", () => {
+        it("should return the employee id", () => {
+            expect(shel.getId()).toBe("123456789");
         });
-        it("should return the employee email from the getEmail() method", () => {
-            expect(employee.getEmail()).toBe("none@none.com");
+    });
+    describe("getEmail()", () => {
+        it("should return the employee email", () => {
+            expect(shel.getEmail()).toBe("none@none.com");
         });
-        it("should return the employee role from the getRole() method", () => {
-            expect(employee.getRole()).toBe("Employee");
+    });
+    describe("getRole()", () => {
+        it("should return the 'Employee' role", () => {
+            expect(shel.getRole()).toBe("Employee");
         });
     });
 });
+
